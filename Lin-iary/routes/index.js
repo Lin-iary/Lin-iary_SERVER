aaf79c4f3210492aadfe72739c592217a5e4ad7c
lumiere <heesung6701@naver.com>
2019-05-06 04:16:24 +0900

    add error

--- Lin-iary/routes/index.js
+++ Lin-iary/routes/index.js
@@ -11,7 +11,7 @@ router.use('/api/consult', require('./api/consult/index'))
 // counselor
 router.use('/api/counselor', require('./api/counselor/index'))
 
-router.use('/image', express.static('public/images'))
-router.use('/image', express.static('public/info'))
+router.use('/images', express.static('public/images'))
+router.use('/images', express.static('public/info'))
 
 module.exports = router
